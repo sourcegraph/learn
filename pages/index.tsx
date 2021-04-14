20e7f400ab9b3e4f889bd3eb39693210dc572e40
marekweb <marekz@gmail.com>
2021-04-14 09:16:13 -0400

    Move index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,13 @@
-import { GetStaticProps } from 'next'
-import Link from 'next/link'
 import React from 'react'
 import PageLayout from '../components/PageLayout'
-import collectTags from '../util/collectTags'
-import listAllPosts from '../util/listAllPosts'
-import loadAllPosts from '../util/loadAllPosts'
 
-interface LinkEntry {
-    title: string
-    url: string
-}
-interface Props {
-    links: LinkEntry[]
-}
-
-export const getStaticProps: GetStaticProps<Props> = async context => {
-    const posts = await loadAllPosts()
-    const postLinks = posts.map(post => ({
-        title: post.frontMatter.title,
-        url: `/posts/${post.filename}`,
-    }))
-    const tags = collectTags(posts)
-    const tagLinks = tags.map(tag => ({
-        title: `Posts tagged "${tag}"`,
-        url: `/tags/${tag}`,
-    }))
-    return {
-        props: {
-            links: [
-                {
-                    title: 'About',
-                    url: '/about',
-                },
-                ...postLinks,
-                ...tagLinks,
-            ],
-        },
-    }
-}
-export default function Home(props: Props) {
+export default function Home() {
     return (
         <PageLayout>
-            <ul>
-                {props.links.map(linkEntry => (
-                    <li key={linkEntry.url}>
-                        <Link href={linkEntry.url}>{linkEntry.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <p>
+                You've found <strong>Sourcegraph Learn</strong>, our new developer education hub.
+            </p>
+            <p>We haven't launched yet! Come back soon.</p>
         </PageLayout>
     )
 }
