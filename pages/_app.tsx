161ca977cae98ef2afd438b9f9cdec6689221595
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-04-16 18:29:57 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import * as gtag from '../lib/gtag'
 export default function MyApp({ Component, pageProps }: AppProps) {
     const router = useRouter()
     useEffect(() => {
-      const handleRouteChange = (url) => {
+      const handleRouteChange = (url: URL) => {
         gtag.pageview(url)
       }
       router.events.on('routeChangeComplete', handleRouteChange)
