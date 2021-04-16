d73793f94fcb12eda0799e1941988e4bff2a494f
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-04-16 18:20:52 -0400

    Adding analytics, changing sample markdown titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 // TODO: update these CSS imports once we settle on a CSS strategy.
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/Footer.scss'
 import '../styles/typography.scss'
+import * as gtag from '../lib/gtag'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }, [router.events])
     return <Component {...pageProps} />
-}
-
-export default MyApp
+  }
