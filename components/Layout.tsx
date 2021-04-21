43b4d25fa5072be0025372f09892d98f0d276d5c
marekweb <marekz@gmail.com>
2021-04-21 11:53:02 -0400

    Move GTM into its own components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import Head from 'next/head'
 import Footer from './Footer'
 import Header from './Header'
+import { GoogleTagManagerScriptTag, GoogleTagManagerNoscriptFrame } from './GoogleTagManager'
+import { googleTagManagerId } from '../posts/site-config'
 
 interface LayoutProps {
     meta?: {
@@ -32,48 +34,30 @@ export default function Layout(props: LayoutProps) {
     const metaProps = { ...defaultMetaProps, ...props.meta }
 
     return (
-        <div className={`flex flex-column fill-height ${props.className || ''}`}>
-            <Head>
-                {/* Google Tag Manager */}
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-TB4NLS7');`,
-                    }}
-                />
-                {/* End Google Tag Manager */}
+        <>
+            <GoogleTagManagerNoscriptFrame id={googleTagManagerId} />
+            <div className={`flex flex-column fill-height ${props.className || ''}`}>
+                <Head>
+                    <GoogleTagManagerScriptTag id={googleTagManagerId} />
 
-                <title>{metaProps.title}</title>
+                    <title>{metaProps.title}</title>
 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link href="https://fonts.googleapis.com/css2?family=PT+Sans" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=IBM+Plex+Sans:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <body>
-                {/* Google Tag Manager (noscript) */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-TB4NLS7"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    ></iframe>
-                </noscript>
-                {/* End Google Tag Manager (noscript) */}
-            </body>
-            <div className={props.heroAndHeaderClassName}>
-                <Header minimal={props.minimal} className={`${props.className || ''}`} />
-                {props.hero}
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <link href="https://fonts.googleapis.com/css2?family=PT+Sans" rel="stylesheet"></link>
+                    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=IBM+Plex+Sans:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+
+                <div className={props.heroAndHeaderClassName}>
+                    <Header minimal={props.minimal} className={`${props.className || ''}`} />
+                    {props.hero}
+                </div>
+                <section className="d-flex flex-column fill-height">{props.children}</section>
+                <Footer minimal={props.minimal} />
             </div>
-            <section className="d-flex flex-column fill-height">{props.children}</section>
-            <Footer minimal={props.minimal} />
-        </div>
+        </>
     )
 }
