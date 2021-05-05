9923cd617c251414fed4175d569693f3f5786d73
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-05-05 18:52:43 -0400

    Promise handling for deployment, enable webpack5 (#26)

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   future: {
-    // Enable webpack5 when it gets fixed to stop emitting:
-    // "Caching failed for pack: Error: Can't resolve 'fsevents'"
-    webpack5: false,
+    // Enable webpack5 
+    webpack5: true,
   },
 })
