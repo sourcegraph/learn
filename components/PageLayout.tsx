12bfcb72df5c8ff8f561e110ae052cfd26a83097
Marek <marek@sourcegraph.com>
2021-05-06 16:15:02 -0400

    Add new navbar (#27)
    * Add icon, add new navbar
    
    * Update layout and columns

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -6,15 +6,17 @@ interface Props {
      */
     contentTitle?: string
     children: React.ReactNode
+    leftColumn?: React.ReactNode
+    rightColumn?: React.ReactNode
 }
 
 export default function PageLayout(props: Props) {
     return (
         <Layout contentTitle={props.contentTitle}>
-            <div className="container">
-                <div className="row">
-                    <div className="col py-5">{props.children}</div>
-                </div>
+            <div className="row">
+                <div className="col-3">{props.leftColumn}</div>
+                <div className="col-6 py-5">{props.children}</div>
+                <div className="col-3">{props.rightColumn}</div>
             </div>
         </Layout>
     )
