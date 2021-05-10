e227375e5ef9c108bb1d6f9a6a728fd53053669f
Marek <marek@sourcegraph.com>
2021-05-10 15:06:17 -0400

    Add auto-generated table of contents (#28)
    * Add table of contents generator
    
    * Remove extra tags
    
    Co-authored-by: ltagliaferri <lisa.tagliaferri@gmail.com>

--- util/renderMdxSource.ts
+++ util/renderMdxSource.ts
@@ -1,7 +1,15 @@
 import renderToString from 'next-mdx-remote/render-to-string'
 import { MdxRemote } from 'next-mdx-remote/types'
 import { MarkdownFile } from './loadMarkdownFile'
+import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
 
 export default function renderMdxSource(markdownFile: MarkdownFile, components: MdxRemote.Components) {
-    return renderToString(markdownFile.body, { components })
+    return renderToString(markdownFile.body, {
+        components,
+        mdxOptions: {
+            remarkPlugins: [remarkToc],
+            rehypePlugins: [rehypeSlug],
+        },
+    })
 }
