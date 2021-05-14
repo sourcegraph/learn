2fc02a480491ec983f8908d63688e4e1fa5ca8f9
Marek <marek@sourcegraph.com>
2021-05-14 12:25:53 -0400

    Improve styling of the search box component (#36)

---
+++ components/SourcegraphSearch.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+interface Props {
+    query: string
+}
+
+export default function SourcegraphSearch(props: Props) {
+    const [query, setQuery] = useState(props.query)
+    return (
+        <div className="my-4">
+            <div className="card">
+                <div className="card-body row">
+                    <div className="col-1">
+                        <img width="32" src="/sourcegraph-mark.svg" />
+                    </div>
+                    <div className="col-7">
+                        <input
+                            type="text"
+                            className="form-control w-100 font-monospace"
+                            value={query}
+                            onChange={event => setQuery(event.target.value)}
+                        />
+                    </div>
+                    <div className="col-4">
+                        <a
+                            className="btn btn-primary"
+                            href={`https://sourcegraph.com/search?q=${encodeURIComponent(query)}`}
+                            target="_blank"
+                        >
+                            <span className="small">Search on Sourcegraph</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
