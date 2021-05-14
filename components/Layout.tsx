58f3aee1fcb3ce8ede308248528ae28cdb5508c3
Marek <marek@sourcegraph.com>
2021-05-14 14:06:52 -0400

    Fix fonts (#37)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -52,10 +52,9 @@ export default function Layout(props: LayoutProps) {
                 <title>{title}</title>
                 <link href="/favicon.png" rel="icon" type="image/png"></link>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link href="https://fonts.googleapis.com/css2?family=PT+Sans" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=IBM+Plex+Sans:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=PT+Sans:wght@700&display=swap"
                     rel="stylesheet"
                 />
             </Head>
