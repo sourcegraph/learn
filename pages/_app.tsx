52038344b6881032afa5a51663107a353d034bfd
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-06-08 23:04:02 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { Tooltip, Toast, Popover } from 'bootstrap';
 // TODO: update these CSS imports once we settle on a CSS strategy.
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
 import '../styles/Footer.scss'
 import '../styles/typography.scss'
