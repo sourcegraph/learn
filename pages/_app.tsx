505b84764c86171ec761c1cc2012db3f7d46f54a
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-06-08 22:19:26 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 // TODO: update these CSS imports once we settle on a CSS strategy.
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
 import '../styles/Footer.scss'
 import '../styles/typography.scss'
