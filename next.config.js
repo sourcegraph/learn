b8f4f37c383edf8e5addd83876c5f8d2e6d9e3e0
Marek <marek@sourcegraph.com>
2021-07-07 13:47:27 -0400

    Clean up dependencies and fix package-lock (#97)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/,
-})
-
-module.exports = withMDX({
+module.exports = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   future: {
     // Enable webpack5
     webpack5: true,
   },
-})
+}
