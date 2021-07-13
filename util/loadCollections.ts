1955633f60e30dbae2e237aabf4d74de8750276a
Marek <marek@sourcegraph.com>
2021-07-13 15:50:08 -0400

    Update error message to include slug

--- util/loadCollections.ts
+++ util/loadCollections.ts
@@ -28,7 +28,7 @@ export default async function loadCollections(): Promise<Collection[]> {
             const memberPost = posts.find(post => post.slug === memberSlug)
             if (!memberPost) {
                 throw new Error(
-                    `Post not found: "" from collection "${collectionDefinition.slug}" (${collectionDefinition.title})`
+                    `Post not found: "${post.slug}" from collection "${collectionDefinition.slug}" (${collectionDefinition.title})`
                 )
             }
             return memberPost
