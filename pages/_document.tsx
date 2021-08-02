3dfc791e31d341506f9f1e5d3a3de3a780923d43
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-02 16:03:39 -0400

    Add initial scaffolding for styled-components

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import React from 'react'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+    public static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = context.renderPage
+    
+        try {
+          context.renderPage = () =>
+            originalRenderPage({
+              enhanceApp: App => props =>
+                sheet.collectStyles(<App {...props} />),
+            })
+    
+          const initialProps = await Document.getInitialProps(context)
+          return {
+            ...initialProps,
+            styles: (
+              <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+              </>
+            ),
+          }
+        } finally {
+          sheet.seal()
+        }
+    }
+}
