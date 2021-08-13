5c2954a3260160c03977ab8ccea7aa2cf4945464
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-13 11:43:33 -0400

    Remove globby

--- lib/listAllRecords.ts
+++ lib/listAllRecords.ts
@@ -1,7 +1,8 @@
-import globby from 'globby'
+import { promises as fs } from 'fs'
 
 export default async function listAllRecords(baseDirectory: string): Promise<string[]> {
-    const pattern = '**/*.{md,markdown,mdx}'
-    const entries = await globby(pattern, { cwd: baseDirectory })
-    return entries
+    const fileRegex = /\.(md|markdown|mdx)$/gi
+    const getFiles = await fs.readdir(baseDirectory)
+    const matchedFiles = getFiles.filter(file => file.match(fileRegex))
+    return matchedFiles
 }
