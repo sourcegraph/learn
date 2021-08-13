23fba1f546b641244b90d6be5c4e04722d0cdbd6
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-13 15:02:41 -0400

    Remove guides and references to guides for now

--- lib/listAllRecords.ts
+++ lib/listAllRecords.ts
@@ -3,11 +3,7 @@ import { promises as fs } from 'fs'
 export default async function listAllRecords(baseDirectory: string): Promise<string[]> {
     const fileRegex = /\.(md|markdown|mdx)$/gi
     const getFiles = await fs.readdir(baseDirectory)
-    if (getFiles.length >=1) {
-        const matchedFiles = getFiles.filter(file => file.match(fileRegex))
+    const matchedFiles = getFiles.filter(file => file.match(fileRegex))
 
-        return matchedFiles
-    }
-
-    return []
+    return matchedFiles
 }
