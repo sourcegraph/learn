83da14e170091e6f6bae9d0bbcac7bf9af8f8513
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-13 14:01:19 -0400

    Add length check to returned records

--- lib/listAllRecords.ts
+++ lib/listAllRecords.ts
@@ -3,7 +3,11 @@ import { promises as fs } from 'fs'
 export default async function listAllRecords(baseDirectory: string): Promise<string[]> {
     const fileRegex = /\.(md|markdown|mdx)$/gi
     const getFiles = await fs.readdir(baseDirectory)
-    const matchedFiles = getFiles.filter(file => file.match(fileRegex))
+    if (getFiles.length >=1) {
+        const matchedFiles = getFiles.filter(file => file.match(fileRegex))
 
-    return matchedFiles
+        return matchedFiles
+    }
+
+    return []
 }
