33301a67f696cbe9fdc858fb5327d84aee80b72a
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-23 09:20:45 -0400

    Add placeholders for env vars to install script

--- script/install.sh
+++ script/install.sh
@@ -6,4 +6,4 @@ npm ci || exit 1
 
 echo "Creating local env file"
 
-printf "NEXT_PUBLIC_URL='http://localhost:3000'\nNEXT_PUBLIC_GTM_ID='GTM-TB4NLS7'" > .env
+printf "NEXT_PUBLIC_URL='http://localhost:3000'\nNEXT_PUBLIC_GTM_ID='GTM-TB4NLS7'\nSEARCH_API_AUTH_TOKEN=''\nSEARCH_API_URL=''" > .env
