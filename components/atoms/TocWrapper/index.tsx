982d19abeee0273bf26c5d82cc0f14293d4392ac
katjuell <kathleenjuell@gmail.com>
2021-08-25 17:24:49 -0400

    Add highlighting on scroll for ToC (#180)
    * Add hook to highlight ToC
    
    * Update on linting
    
    * Fix highlight on bottom elements and adjust positioning on ToC
    
    * Resolve remaining linting errors and update toc on record to remove extra space
    
    * Adjust rootMargin for firefox and use alias for hooks dir
    
    * Add breakpoint for ToC position
    
    * Breakpoints for narrowing ToC
    
    * Breakpoints for footer padding
    
    * Adjustments to padding on footer for breakpoints
    
    * Change breakpoint for visibility on ToC

--- components/atoms/TocWrapper/index.tsx
+++ components/atoms/TocWrapper/index.tsx
@@ -1,47 +1,72 @@
+import useHighlightOnScroll from '@hooks/highlightOnScroll'
 import convertHeaders from '@util/convertHeaders'
 import createRandomId from '@util/createRandomId'
 import sluggify from '@util/sluggify'
 import sluggifyHeaders from '@util/sluggifyHeaders'
 import Link from 'next/link'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect, useState } from 'react'
 
 import { 
+    StyledTocWrapperBody,
     StyledTocWrapper,
+    StyledTocTopWrapper,
     StyledHeaderTocItem,
     StyledTocItem,
  } from './TocWrapperStyles'
 
 interface Props {
-    tocContents: string[],
+    tocContents: string[]
     slug: string
 }
 
 const TocWrapper: FunctionComponent<Props> = props => {
+    const [elements, setElements] = useState<Element[] | null>(null)
     const convertedHeaders = convertHeaders(props.tocContents)
+    const highlightHook = useHighlightOnScroll(elements)
+    useEffect(() => {
+        if (props.tocContents) {
+            const getElements = [].slice.call(document.querySelectorAll('h2, h3'))
+            setElements(getElements)
+        }
+    }, [props.tocContents])
+
+    useEffect(() => {
+        if (elements) {
+            highlightHook.setHeaders(elements)
+        }
+    }, [elements, highlightHook])
 
     return (
-        <StyledTocWrapper>
-            <h5>Contents</h5>
-                <ul>
-                    {convertedHeaders.map(header =>
-                        header.isNested
-                        ?  (
-                            <StyledTocItem key={createRandomId()}>
-                                <Link href={`/${props.slug}/#${sluggify(header.header)}`}>
-                                    <a>{header.header}</a>
-                                </Link>  
-                            </StyledTocItem>
-                        )
-                        : (
-                            <StyledHeaderTocItem key={createRandomId()}>
-                                <Link href={`/${props.slug}/#${sluggifyHeaders(sluggify(header.header))}`}>
-                                    <a>{header.header}</a>
-                                </Link>   
-                            </StyledHeaderTocItem>
-                        )
-                    )}
-                </ul>
-        </StyledTocWrapper>
+        <StyledTocTopWrapper>
+            <StyledTocWrapper>
+                <StyledTocWrapperBody>
+                    <h5>Contents</h5>
+                        <ul>
+                            {convertedHeaders.map(header =>
+                                header.isNested
+                                ?  (
+                                    <StyledTocItem 
+                                        key={createRandomId()}
+                                        isHighlighted={highlightHook.activeHeader === `${sluggify(header.header)}`}>
+                                        <Link href={`/${props.slug}/#${sluggify(header.header)}`}>
+                                            <a>{header.header}</a>
+                                        </Link>  
+                                    </StyledTocItem>
+                                )
+                                : (
+                                    <StyledHeaderTocItem
+                                        key={createRandomId()}
+                                        isHighlighted={highlightHook.activeHeader === `${sluggifyHeaders(sluggify(header.header))}`}>
+                                        <Link href={`/${props.slug}/#${sluggifyHeaders(sluggify(header.header))}`}>
+                                            <a>{header.header}</a>
+                                        </Link>   
+                                    </StyledHeaderTocItem>
+                                )
+                            )}
+                        </ul>
+                </StyledTocWrapperBody>
+            </StyledTocWrapper>
+        </StyledTocTopWrapper>
     )
 }
 
