7acc9e83fcb9835e68ecc0870f46bc7cdd46af6a
Kathleen Juell <kathleenjuell@gmail.com>
2021-08-30 12:34:37 -0400

    Remove width setting from column atom

--- components/atoms/Column/ColumnStyles.ts
+++ components/atoms/Column/ColumnStyles.ts
@@ -1,29 +1,7 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
-interface Props {
-    width?: string
-}
-
-const setColumnWidth = (width: string | undefined): string => {
-    if (width) {
-        switch (width) {
-            case 'extra-small':
-                return '8.33%'
-            case 'small':
-                return '33.33%'
-            case 'medium':
-                return '58.33%'
-            case 'large':
-                return '83.33%'
-        }
-    }
-
-    return ''
-}
-
-export const StyledColumn = styled.div<Props>`
+export const StyledColumn = styled.div`
     display: flex;
-    width: ${props => setColumnWidth(props.width)}
 
     @media screen and (max-width: 768px) {
         width: 100%;
