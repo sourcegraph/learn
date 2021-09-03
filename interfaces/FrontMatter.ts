ce97f2b6e52bd5a279438332735afca7a3dfabc6
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-09-02 22:32:09 -0400

    Update FrontMatter.ts

--- interfaces/FrontMatter.ts
+++ interfaces/FrontMatter.ts
@@ -13,7 +13,7 @@ export default interface FrontMatter {
     author?: string | null
 
     /** First publication date and updated publication date */
-    publicationDate: string | null
+    publicationDate?: string | null
     updatedDate?: string | null
 
     /** Short description used for the post's card and social description meta
