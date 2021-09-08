8c3bcf1a20a2bb87c52cd5305233201c52e83eeb
Kathleen Juell <kathleenjuell@gmail.com>
2021-09-08 15:24:04 -0400

    Update install script

--- script/install.sh
+++ script/install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-echo "Installing dependencies from lock file"
+echo "Installing dependencies from lock file for client"
 
 npm ci || exit 1
 
-echo "Creating local env file"
+echo "Creating local env file for client"
 
-printf "NEXT_PUBLIC_URL='http://localhost:3000'\nNEXT_PUBLIC_GTM_ID='GTM-TB4NLS7'\nNEXT_PUBLIC_SEARCH_API_AUTH_TOKEN=''\nSNEXT_PUBLIC_SEARCH_API_URL=''" > .env
+printf "NEXT_PUBLIC_URL='http://localhost:3000'\nNEXT_PUBLIC_GTM_ID='GTM-TB4NLS7'" > .env
+
+echo "Installing dependencies from lock file for backend"
+
+cd backend
+npm ci || exit 1
+
+echo "Creating local env file for client"
+
+printf "ORIGIN_URL='http://localhost:3000'\nSEARCH_API_AUTH_TOKEN=''\nSEARCH_API_URL=''" > .env
