c0432d5131497d85b00536bb449973b8cb9de59f
Kathleen Juell <kathleenjuell@gmail.com>
2021-09-23 10:24:33 -0400

    Add results table component to display search results, and ensure structural search results display correctly

--- client/components/atoms/InteractiveSearchResults/InteractiveSearchResultsStyles.ts
+++ client/components/atoms/InteractiveSearchResults/InteractiveSearchResultsStyles.ts
@@ -6,28 +6,3 @@ export const StyledResultsCodeContainer = styled.div`
     overflow: scroll;
     padding: .25rem 0;
 `
-export const StyledResultsCodeBlock = styled.div`
-    background: #e7e7e7;
-    border: 1px solid #ced4da;
-    border-radius: .25rem;
-    overflow-x: auto;
-`
-export const StyledResultsCodeTable = styled.table`
-    border-collapse: collapse;
-    border-spacing: 0px;
-    table-layout: fixed;
-    width: 34.35rem;
-`
-export const StyledResultsCodeLineNumber = styled.td`
-    height: .88rem; 
-    padding-right: .75rem;
-    width: 5%;
-`
-export const StyledResultsCodeLine = styled.td`
-    height: .88rem;
-    vertical-align: middle;
-    padding: 0 0 0 1rem;
-    text-align: left;
-    white-space: pre;
-    word-break: break-all;
-`
--- client/components/atoms/InteractiveSearchResults/index.tsx
+++ client/components/atoms/InteractiveSearchResults/index.tsx
@@ -1,16 +1,8 @@
-import Highlighter from '@components/atoms/Highlighter'
+import SearchResultsTable from '@components/atoms/SearchResultsTable'
 import { ResultsObject, LineMatch } from '@interfaces/Search'
-import createRandomId from '@util/createRandomId'
-import { returnPreviousLine, returnNextLine }from '@util/returnLineMatchContext'
 import { FunctionComponent } from 'react'
 
-import {
-    StyledResultsCodeTable,
-    StyledResultsCodeContainer,
-    StyledResultsCodeBlock,
-    StyledResultsCodeLineNumber,
-    StyledResultsCodeLine,
-} from './InteractiveSearchResultsStyles'
+import { StyledResultsCodeContainer } from './InteractiveSearchResultsStyles'
 
 interface Props {
     result: ResultsObject
@@ -22,38 +14,14 @@ interface Props {
 const InteractiveSearchResults: FunctionComponent<Props> = props => (
     <StyledResultsCodeContainer>
         {props.lineMatches.length > 0 && (
-            props.lineMatches.slice(0,4).map((line: LineMatch) => (
-                <StyledResultsCodeBlock key={createRandomId()}>
-                    <StyledResultsCodeTable>
-                        <tbody>
-                            <tr>
-                                {line.lineNumber && line.lineNumber > 0 &&
-                                <>
-                                    <StyledResultsCodeLineNumber>{line.lineNumber}</StyledResultsCodeLineNumber>
-                                    <StyledResultsCodeLine>
-                                        {returnPreviousLine(props.result.file.content, line.lineNumber)}
-                                    </StyledResultsCodeLine>
-                            </>}                                         
-                            </tr>   
-                            <tr>
-                                <StyledResultsCodeLineNumber>{line.lineNumber + 1}</StyledResultsCodeLineNumber>
-                                <StyledResultsCodeLine>
-                                    <Highlighter
-                                        input={line.preview}
-                                        matcher={props.query}
-                                    />
-                                </StyledResultsCodeLine>
-                            </tr>
-                            <tr>
-                                <StyledResultsCodeLineNumber>{line.lineNumber + 2}</StyledResultsCodeLineNumber>
-                                <StyledResultsCodeLine>
-                                    {returnNextLine(props.result.file.content, line.lineNumber)}
-                                </StyledResultsCodeLine>
-                            </tr>  
-                        </tbody>
-                    </StyledResultsCodeTable>
-                </StyledResultsCodeBlock>
-            ))
+            <SearchResultsTable
+                matches={props.patternType === 'literal'
+                    ? props.lineMatches.slice(0,4)
+                    : props.lineMatches}
+                result={props.result}
+                patternType={props.patternType}
+                query={props.query}
+            />
         )}
     </StyledResultsCodeContainer>
 )
