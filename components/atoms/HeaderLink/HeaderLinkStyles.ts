ca1a0bcb539a3def2ee735b6683136f68f668186
katjuell <kathleenjuell@gmail.com>
2021-10-06 16:23:35 -0400

    Render code in ToC headers (#260)
    * Add HeaderLink component to render code in ToC headers
    
    * Add font-family for code and resolve key warnings on loop
    
    * Update import statement on ToC Wrapper
    
    * Remove unneeded space from code header display

---
+++ components/atoms/HeaderLink/HeaderLinkStyles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const StyledLink = styled.a`
+    cursor: pointer;
+    text-decoration: none;
+`
+export const StyledLinkSpan = styled.span`
+    background-color: transparent;
+`
+export const StyledLinkCodeSpan = styled.span`
+    background-color: #e7e7e7;
+    font-family: Monaco, monospace; 
+`
---
+++ components/atoms/HeaderLink/index.tsx
@@ -0,0 +1,34 @@
+import createRandomId from '@util/createRandomId'
+import sluggify from '@util/sluggify'
+import sluggifyHeaders from '@util/sluggifyHeaders'
+import Link from 'next/link'
+import { FunctionComponent } from 'react'
+
+import { StyledLink, StyledLinkSpan, StyledLinkCodeSpan } from './HeaderLinkStyles'
+
+interface Props {
+    header: string
+    slug: string
+    isNested: boolean
+}
+
+const HeaderLink: FunctionComponent<Props> = props => {
+    const headers = props.header.split(' ')
+    const backtickRegex = /`/gi
+
+    return (
+        <Link href={props.isNested
+            ? `/${props.slug}/#${sluggify(props.header)}`
+            : `/${props.slug}/#${sluggifyHeaders(sluggify(props.header))}`} passHref={true}>
+            <StyledLink>
+                {headers.map(header => (
+                    header.includes('`')
+                        ? <StyledLinkCodeSpan key={createRandomId()}>{' '}{header.replace(backtickRegex, '')}</StyledLinkCodeSpan>
+                        : <StyledLinkSpan key={createRandomId()}>{' '}{header}{' '}</StyledLinkSpan>
+                ))}
+            </StyledLink>
+        </Link>
+    )
+}
+
+export default HeaderLink
