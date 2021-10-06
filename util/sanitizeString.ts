d1d326065403b97bea0647c140eeeb01f38c863d
Kathleen Juell <kathleenjuell@gmail.com>
2021-10-06 13:43:45 -0400

    Trim all parts of query input

--- util/sanitizeString.ts
+++ util/sanitizeString.ts
@@ -2,7 +2,11 @@ const sanitizeString = (string: string): string => {
     // Regex for zero-width space characters
     const zeroWidthRegex = /\u200B/g
 
-    return string.trim().replace(zeroWidthRegex, '')
+    return string
+        .split(' ')
+        .filter(item => item !== '')
+        .map(item => item.replace(zeroWidthRegex, ''))
+        .join(' ')
 }
 
 export default sanitizeString
