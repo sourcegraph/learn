e97d52fdaf5e85b1ea3508d8217260241bdfd3a8
katjuell <kathleenjuell@gmail.com>
2021-10-12 09:59:15 -0400

    Move GTM to _app, reorg _document and move prism and font stylesheets there as well (#269)

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,4 @@
 import Container from '@components/atoms/Container'
-import { GoogleTagManagerScriptTag, GoogleTagManagerNoscriptFrame } from '@components/atoms/GoogleTagManager'
 import Footer from '@components/Footer'
 import NavBar from '@components/NavBar'
 import MetaTags from '@interfaces/MetaTags'
@@ -12,20 +11,9 @@ export interface Props {
 
 const Layout: FunctionComponent<Props> = props => (
     <>
-        <GoogleTagManagerNoscriptFrame />
-        <GoogleTagManagerScriptTag />
         <Head>
             <title>{props.metaTags.title}</title>
-            <link href="https://storage.googleapis.com/sourcegraph-assets/learn/favicon.png" rel="icon" type="image/png" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=PT+Sans:wght@700&display=swap"
-            />
-            {/* Prism theme for syntax highlighting */}
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.4.1/themes/prism.min.css" />
-
             <meta property="og:type" content={props.metaTags.type} />
             <meta property="og:title" content={props.metaTags.title} />
             <meta property="og:image" content={props.metaTags.image} />
