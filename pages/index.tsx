782936a177a3133492a0e0685e36a6015664434c
Kathleen Juell <kathleenjuell@gmail.com>
2021-10-19 15:27:46 -0400

    Update homepage to swap out videos

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,13 @@ import { GetStaticProps } from 'next'
 
 export const getStaticProps: GetStaticProps<HomepageTemplateProps> = async () => {
     const posts = await loadAllRecords('posts')
-    const videos = await loadAllRecords('videos')
     const searchPosts = posts.filter(record => record.frontMatter.tags.includes('search')).slice(0,3)
-    const videoPosts = videos.filter(record => record.frontMatter.tags.includes('sourcegraph')).slice(0,3)
+    const sourcegraphPosts = posts.filter(record => record.frontMatter.tags.includes('sourcegraph')).slice(0,3)
 
     return {
         props: {
             searchPosts: searchPosts.map(post => omitUndefinedFields({ ...post, url: `/${post.slug}` })),
-            videoPosts: videoPosts.map(video => omitUndefinedFields({ ...video, url: `/${video.slug}` })),
+            sourcegraphPosts: sourcegraphPosts.map(video => omitUndefinedFields({ ...video, url: `/${video.slug}` })),
         },
     }
 }
