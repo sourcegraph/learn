8f2f9fe82335e8df44d9ad6f30c6349dece38fca
Kathleen Juell <kathleenjuell@gmail.com>
2021-11-18 17:50:00 -0500

    Move robots img to bucket

--- components/templates/ErrorTemplate/index.tsx
+++ components/templates/ErrorTemplate/index.tsx
@@ -15,8 +15,8 @@ const ErrorTemplate: FunctionComponent = () => {
     return (
         <StyledErrorPageWrapper>
             <StyledErrorPageImageHeader>
-                <img alt='Sourcegraph robots' src='/static/images/sg_robots.svg' />
-            </StyledErrorPageImageHeader>  
+                <img alt='Sourcegraph robots' src='https://storage.googleapis.com/sourcegraph-assets/learn/images/sg_robots.svg' />
+            </StyledErrorPageImageHeader>
             <StyledHeaderText>Not what you were ... searching for?</StyledHeaderText>
             <StyledErrorPageText isDark={theme.isDark}>
                 Head back over to the {' '}
