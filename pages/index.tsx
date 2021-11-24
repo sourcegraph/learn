1fa79cb63e3fc662c855a0db1b1229f39c41111a
Kathleen Juell <kathleenjuell@gmail.com>
2021-11-24 08:11:25 -0500

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import HomepageTemplate, { Props as HomepageTemplateProps } from '@components/templates/HomepageTemplate'
-import loadAllRecords from '@lib/loadAllRecords'
-import omitUndefinedFields from '@util/omitUndefinedFields'
-import returnHomepageContent from '@util/returnHomepageContent'
+import { PageData } from '@interfaces/PageData'
+import { getPageData } from '@lib/api/getPageData'
+import filterRecordsWithTag from '@util/filterRecordsWithTag'
 import { GetStaticProps } from 'next'
 
 export const getStaticProps: GetStaticProps<HomepageTemplateProps> = async () => {
-    const posts = await loadAllRecords('posts')
-    const searchPosts = returnHomepageContent(posts, 'search')
-    const sourcegraphPosts = returnHomepageContent(posts, 'sourcegraph')
+    const totalRecords = await getPageData() as PageData
+    const posts = totalRecords.records.posts ?? []
+    const searchPosts = filterRecordsWithTag(posts, 'search').records.slice(0, 3)
+    const sourcegraphPosts = filterRecordsWithTag(posts, 'sourcegraph').records.slice(0, 3)
 
     return {
         props: {
-            searchPosts: searchPosts.map(post => omitUndefinedFields({ ...post, url: `/${post.slug}` })),
-            sourcegraphPosts: sourcegraphPosts.map(video => omitUndefinedFields({ ...video, url: `/${video.slug}` })),
+            searchPosts,
+            sourcegraphPosts,
         },
     }
 }
